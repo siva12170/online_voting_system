f56713309c3f24016ba9fe932738b50b1303a2d0
Kapil Soni <78656206+kapil4457@users.noreply.github.com>
2022-02-06 23:41:33 +0530

    Add files via upload

---
+++ backend/src/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URI , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Connected to database')
+}).catch((err)=>{
+    console.log('Could not connect to database')
+})
